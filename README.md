b2be9ac99b0e54f080420a5438a16b306c91b7b8
Fischbrot <stjesner@gmail.com>
2020-04-13 20:43:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# NPARSE!
+
+This module is created as a CLI tool for parsing values from json - files 
+(my use-case was having a cli-tool that can do this for CI jobs)
+
+### USAGE:
+```
+nparse -f relative-path-to-JSON "1st-level>2nd-level>3rd-level..."
+nparse -a absolute-path-to-JSON "1st-level"
+```
+
+
+License: MIT
---
+++ bin/app.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+const fs = require("fs")
+let file = ""
+
+if(process.argv[2] == "-f") {
+	file = process.cwd() + "\\" + process.argv[3]
+} else if(process.argv[2] == "-a") {
+	file = process.argv[3]
+} else if(process.argv[2] == "-h" || process.argv[2] == "--help") {
+	console.log("nparse -f (relative path to JSON)")
+	console.log("nparse -a (absolute path to JSON)")
+	process.exit(0)
+}
+
+if(process.argv[4] == undefined) {
+	process.exit(1)
+}
+
+fs.readFile(file, 'utf8', (err, data) => {
+    if (err) reject(err)
+    let json = JSON.parse(data)
+	let out = json;
+	let query = process.argv[4].split(">");
+
+	query.forEach(function(curr_step) {
+		out = out[curr_step]
+	})
+
+	process.stdout.write(out)
+})
